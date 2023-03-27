0969163bbd1d5ad9980204bc872b80069bd963e5
erik-ferreira <erikdesouzagf@gmail.com>
2023-03-27 14:24:05 -0300

    add stitches to work with css

---
+++ src/styles/index.ts
@@ -0,0 +1,18 @@
+import { createStitches } from "@stitches/react";
+
+export const {
+  css,
+  config,
+  styled,
+  globalCss,
+  keyframes,
+  getCssText,
+  theme,
+  createTheme,
+} = createStitches({
+  theme: {
+    colors: {
+      rocket: "#8759c1",
+    },
+  },
+});
