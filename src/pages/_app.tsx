3677b0ee358c012698eeffaa0497cd8d2efdf4eb
erik-ferreira <erikdesouzagf@gmail.com>
2023-03-27 14:59:38 -0300

    complete config global styles

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,3 +1,22 @@
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+import { AppProps } from "next/app";
+import Image from "next/image";
+
+import logoImg from "../assets/logo.svg";
+
+import { globalStyles } from "../styles/global";
+
+import { ContainerApp, Header } from "../styles/pages/app";
+
+globalStyles();
+
+export default function App({ Component, pageProps }: AppProps) {
+  return (
+    <ContainerApp>
+      <Header>
+        <img src={logoImg.src} alt="" />
+      </Header>
+
+      <Component {...pageProps} />
+    </ContainerApp>
+  );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,7 @@
 import { styled } from "../styles";
 
 const Button = styled("button", {
-  backgroundColor: "$rocket",
+  backgroundColor: "$green500",
   borderRadius: 6,
   border: 0,
   padding: "4px 8px",
@@ -17,12 +17,9 @@ const Button = styled("button", {
 
 export default function Home() {
   return (
-    <>
-      <h1>Hello world</h1>
-      <Button>
-        <span>dale</span>
-        teste
-      </Button>
-    </>
+    <Button>
+      <span>dale</span>
+      teste
+    </Button>
   );
 }
