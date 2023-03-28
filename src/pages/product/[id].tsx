d841f178fd407154009895f9e75c74d77c788893
erik-ferreira <erikdesouzagf@gmail.com>
2023-03-28 15:46:11 -0300

    complete structure and style product

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -1,7 +1,32 @@
+import Image from "next/image";
 import { useRouter } from "next/router";
+
+import {
+  ImageContainer,
+  ProductContainer,
+  ProductDetails,
+} from "../../styles/pages/product";
 
 export default function Product() {
   const { query } = useRouter();
 
-  return <h1>{JSON.stringify(query)}</h1>;
+  return (
+    <ProductContainer>
+      <ImageContainer>{/* <Image src="" alt="" /> */}</ImageContainer>
+
+      <ProductDetails>
+        <h1>Camiseta X</h1>
+        <span>R$ 79,90</span>
+
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam
+          temporibus magnam voluptates voluptatibus! Labore natus accusamus
+          recusandae quisquam unde consequuntur perferendis eum ipsam architecto
+          officia tenetur cum, ea explicabo adipisci.
+        </p>
+
+        <button>Comprar agora</button>
+      </ProductDetails>
+    </ProductContainer>
+  );
 }
