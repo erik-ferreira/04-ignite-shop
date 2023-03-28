afc8ac0137da0168c935732f0f9488e9648e90fe
erik-ferreira <erikdesouzagf@gmail.com>
2023-03-28 17:16:27 -0300

    add ssr in page details product and getStaticPaths

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -1,5 +1,9 @@
+import Stripe from "stripe";
 import Image from "next/image";
 import { useRouter } from "next/router";
+import { GetStaticPaths, GetStaticProps } from "next";
+
+import { stripe } from "../../lib/stripe";
 
 import {
   ImageContainer,
@@ -7,26 +11,73 @@ import {
   ProductDetails,
 } from "../../styles/pages/product";
 
-export default function Product() {
-  const { query } = useRouter();
+interface ProductProps {
+  product: {
+    id: string;
+    name: string;
+    imageUrl: string;
+    price: string;
+    description: string;
+  };
+}
+
+export default function Product({ product }: ProductProps) {
+  const { isFallback } = useRouter();
+
+  if (isFallback) {
+    return <p>Loading...</p>;
+  }
 
   return (
     <ProductContainer>
-      <ImageContainer>{/* <Image src="" alt="" /> */}</ImageContainer>
+      <ImageContainer>
+        <Image src={product.imageUrl} width={520} height={480} alt="" />
+      </ImageContainer>
 
       <ProductDetails>
-        <h1>Camiseta X</h1>
-        <span>R$ 79,90</span>
+        <h1>{product.name}</h1>
+        <span>{product.price}</span>
 
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam
-          temporibus magnam voluptates voluptatibus! Labore natus accusamus
-          recusandae quisquam unde consequuntur perferendis eum ipsam architecto
-          officia tenetur cum, ea explicabo adipisci.
-        </p>
+        <p>{product.description}</p>
 
         <button>Comprar agora</button>
       </ProductDetails>
     </ProductContainer>
   );
 }
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  return {
+    paths: [{ params: { id: "prod_Nbg1ojWTnjIZeb" } }],
+    fallback: true,
+  };
+};
+
+export const getStaticProps: GetStaticProps<any, { id: string }> = async ({
+  params,
+}) => {
+  const productId = params.id;
+
+  const productStripe = await stripe.products.retrieve(productId, {
+    expand: ["default_price"],
+  });
+
+  const defaultPrice = productStripe.default_price as Stripe.Price;
+  const price = new Intl.NumberFormat("pt-BR", {
+    style: "currency",
+    currency: "BRL",
+  }).format(defaultPrice.unit_amount / 100);
+
+  const product = {
+    id: productStripe.id,
+    name: productStripe.name,
+    imageUrl: productStripe.images[0],
+    price,
+    description: productStripe.description,
+  };
+
+  return {
+    props: { product },
+    revalidate: 60 * 60 * 1, // 1 hour
+  };
+};
