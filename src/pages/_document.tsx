d79a486e9d74999cd4450e868f6074e7996bf831
erik-ferreira <erikdesouzagf@gmail.com>
2023-03-29 17:46:44 -0300

    add favicon

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -16,6 +16,8 @@ export default function Document() {
           rel="stylesheet"
         />
 
+        <link rel="shortcut icon" href="favicon.png" type="image/png" />
+
         <style
           id="stitches"
           dangerouslySetInnerHTML={{ __html: getCssText() }}
